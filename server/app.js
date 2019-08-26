3aac02a7f67c4489a558c25c887084fe7e8b199a
bunmin <feri@inamart.co.id>
2019-08-26 15:24:12 +0700

    initial commit

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//connect to mlab database
+// mongoose.connect('mongodb+srv://admin:admin@cluster0-veiz3.mongodb.net/test?retryWrites=true&w=majority',{ useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/');
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+});
+
+app.use('/graphql',graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+app.listen(4000, () => {
+    console.log('now starting on the port 4000');
+});
